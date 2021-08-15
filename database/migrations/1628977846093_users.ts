1a94dc432f2be4027fad34e00ab2b30102b8de01
Eric Ueta <eric-ueta@hotmail.com>
2021-08-14 23:05:34 -0300

    fix[migrations]: adicionado not nullable, project clean

--- database/migrations/1628977846093_users.ts
+++ database/migrations/1628977846093_users.ts
@@ -10,12 +10,12 @@ export default class Users extends BaseSchema {
       table.string('password', 50).notNullable()
       table.string('email', 100).notNullable().unique()
       table.string('emailToken', 100).nullable()
-      table.text('about').notNullable()
+      table.text('about').nullable()
       table.integer('phone', 9).notNullable()
       table.datetime('birth').notNullable()
       table.string('gender', 1).notNullable()
       table.string('notificationToken', 150).nullable()
-      table.integer('cityId').unsigned().references('id').inTable('city')
+      table.integer('cityId').unsigned().references('id').inTable('city').notNullable()
       table.integer('preferenceId').unsigned().references('id').inTable('preference')
       /**
        * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
